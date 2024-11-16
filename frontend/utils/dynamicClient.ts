6b013e96ff1dcd1dfa8da561787c39e28621866d
6luv <jmj00005@naver.com>
2024-11-16 19:27:12 +0700

    feat: dynamic wallet

---
+++ frontend/utils/dynamicClient.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@dynamic-labs/client";
+import { ReactNativeExtension } from "@dynamic-labs/react-native-extension";
+import { DYNAMIC_ENVIRONMENT_ID } from "@env";
+
+if (!DYNAMIC_ENVIRONMENT_ID) {
+  throw new Error("DYNAMIC_ENVIRONMENT_ID is not defined in .env file.");
+}
+
+export const dynamicClient = createClient({
+  environmentId: DYNAMIC_ENVIRONMENT_ID,
+}).extend(ReactNativeExtension());
